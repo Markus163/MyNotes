dccc9ca7a54c5a2f5ca0fa49de6c8aa02e8f45a8
Mark Mikhailov <pprimak@mail.ru>
2022-12-22 15:13:17 +0200

    Add interface

---
+++ MyNotes/MainTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MainTableViewController.swift
+//  MyNotes
+//
+//  Created by Марк Михайлов on 22.12.2022.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+    
+    var notes = [ "Hello! It's your first note."
+            //"Apple Park Visit\n\nRemember to pack food for the long road trip!\n\nWe will need...\n\n- Protein Bars\n- Plently of Water\n- Apples\n\nAddress is 1 Apple Park Way"
+        ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = notes[indexPath.row]
+        cell.detailTextLabel?.text = "31.12.2023"
+        return cell
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
